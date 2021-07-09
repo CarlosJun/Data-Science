1b285d9a8b0629fd7904352bc5bfcac8665285c7
Carlos <franciscocarlosfnj@gmail.com>
2021-07-08 21:50:14 -0300

    feat: add user to database

--- Web API/Flask/example_3_database/client.py
+++ Web API/Flask/example_3_database/client.py
@@ -1,9 +1,15 @@
 import requests
 
+# # requisição simples com get
+# response = requests.get('http://0.0.0.0:5000/empregados/salario/5000')
+
+# requisição com usuário e senha usando post
 data = {"username": "Carlos", "secret":"@dmin456", "info":"salario", "value":5000}
 response = requests.post('http://0.0.0.0:5000/informations', data=data)
 
-# response = requests.get('http://0.0.0.0:5000/empregados/salario/5000')
+# # inserção de dados com usuário e senha usando post
+# data = {"username": "Carlos", "secret":"@dmin456", "nome":"Gabriel", "cargo":"Engenheiro", "salario":"6000"}
+# response = requests.post('http://0.0.0.0:5000/register', data=data)
 
 if response.status_code == 200:
 	message = response.json()
--- Web API/Flask/example_3_database/server.py
+++ Web API/Flask/example_3_database/server.py
@@ -117,5 +117,34 @@ def get_empregados_post():
 					
 	return {'empregados': employers}
 
+@app.route("/register", methods=['POST'])
+def add_empregados_post():
+		
+	username = request.form['username']
+	secret = request.form['secret']
+
+	if not check_user(username, secret):
+		# 401 HTTP Unauthorized
+		return Response("Unauthorized", status=401)
+
+	nome = request.form['nome']
+	cargo = request.form['cargo']
+	salario = request.form['salario']
+
+	# construir a consulta
+	query = """
+            	INSERT INTO empregados (nome, cargo, salario)
+            	VALUES ("{}", "{}", "{}");
+            """.format(nome, cargo, salario)
+
+	# definindo um cursor
+	cursor = g.conn.cursor()
+    # lendo os dados
+	cursor.execute(query)
+	# salvando os dados no banco
+	g.conn.commit()
+					
+	return {"empregados":"Registered employee"}
+
 if __name__ == "__main__":
 	app.run(debug=True)
